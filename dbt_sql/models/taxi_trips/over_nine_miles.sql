602fd931bc892dc86ff198dee030562746e082c1
Rellikson <kisyular@msu.edu>
2026-01-16 22:04:36 -0500

    TESTING MATERIALIZATION TYPE, {{ config(materialized='table') VS {{ config(materialized='view'). BY DEFAULT, IF NOT SPECIFIED, IT DEFAULTS TO view

--- dbt_sql/models/taxi_trips/over_nine_miles.sql
+++ dbt_sql/models/taxi_trips/over_nine_miles.sql
@@ -1,4 +1,8 @@
 -- over_nine_miles: All columns with taxi trip with distance over 9 miles
+
+-- lets materialize as table
+{{ config(materialized='table') }}
+
 SELECT
     *
 FROM {{ ref('taxi_trips') }}
